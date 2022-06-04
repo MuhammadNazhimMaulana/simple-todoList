fc11800f802bc3bb9f5c693002bb98822383f6d2
Bone <nazhimmaulanamuhammad@gmail.com>
2022-06-04 18:21:36 +0700

    Start to add Header

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -2,7 +2,22 @@
 
 @section('container')
 
-<h1>Halo Dunia</h1>
+{{-- Header Section --}}
+<header class="vh-100 d-flex align-items-center" id="home">
+    <div class="container">
+        <div class="row g-5 align-items-center">
+            <div class="col-lg-5 col-md-6">
+                <h1 class="display-5 fw-bold">Menyediakan Layanan Pembuatan Jadwal</h1>
+                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, excepturi.</p>
+                <p class="lead">Lorem ipsum dolor sit amet.</p>
+                <a href="#" class="btn btn-primary">Belajar Lebih</a>
+            </div>
+            <div class="col-lg-7 col-md-6">
+                <img src="" alt="" class="img-fluid shadow-lg">
+            </div>
+        </div>
+    </div>
+</header>
 
 @endsection
 
